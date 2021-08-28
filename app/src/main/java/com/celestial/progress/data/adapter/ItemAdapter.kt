b25fb52747793aab64d9116ba2be7b463ddb63d6
waiyanhtat77@gmail.com <waiyan@1cube.my>
2021-08-28 21:25:38 +0630

    Recycler View Adapter Added Fragemnt View Binding Added

--- app/src/main/java/com/celestial/progress/data/adapter/ItemAdapter.kt
+++ app/src/main/java/com/celestial/progress/data/adapter/ItemAdapter.kt
@@ -1,30 +1,38 @@
 package com.celestial.progress.data.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.celestial.progress.R
+import com.celestial.progress.data.model.Counter
+import com.celestial.progress.databinding.ProgressItemBinding
 
 class ItemAdapter(): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    var itemList = ArrayList<Counter>()
 
 
-
-
-
-    class ItemViewHolder(item: View): RecyclerView.ViewHolder(item){
-
+    init {
 
     }
 
+    inner class ItemViewHolder(val binding: ProgressItemBinding): RecyclerView.ViewHolder(binding.root)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val binding = ProgressItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding)
+
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       with(holder){
+            this.binding.itemTitleId.text = position.toString()
+
+       }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return 5
     }
 }
--- app/src/main/java/com/celestial/progress/ui/DashboardFragment.kt
+++ app/src/main/java/com/celestial/progress/ui/DashboardFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.celestial.progress.R
+import com.celestial.progress.data.adapter.ItemAdapter
+import com.celestial.progress.databinding.FragmentDashboardBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,6 +17,11 @@ private const val ARG_PARAM2 = "param2"
 
 
 class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? =null
+
+    private val binding get() = _binding!!
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,9 +37,19 @@ class DashboardFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.counterRcy.adapter = ItemAdapter()
+        binding.counterRcy.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
