e791df9bee0633cc854f30d8837102407479bb14
waiyanhtat77@gmail.com <waiyan@1cube.my>
2021-09-30 22:46:00 +0630

    Firebase Integration with Analytics and Crashlytics

--- app/src/main/java/com/celestial/progress/MainActivity.kt
+++ app/src/main/java/com/celestial/progress/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.celestial.progress
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.celestial.progress.ui.adapter.ItemAdapter
 import com.celestial.progress.data.model.Counter
 import com.celestial.progress.databinding.ActivityMainBinding
-import com.celestial.progress.others.NotificationHelper
 import com.celestial.progress.others.Status
-import com.celestial.progress.others.Utils
-import com.celestial.progress.others.Utils.showNotification
 import com.celestial.progress.ui.CounterViewModel
+import com.celestial.progress.ui.adapter.ItemAdapter
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -38,26 +36,45 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: CounterViewModel
 
-    var insertCounter: (Counter,()->Unit) -> Unit = { counter ,func ->
-        lifecycleScope.launch {
-            viewModel.createCounter(counter).observe(this@MainActivity, androidx.lifecycle.Observer {
-                when(it.status){
-                    Status.SUCCESS ->{
-                        func.invoke()
-                        binding?.mainLayout?.let { it1 -> Snackbar.make(it1,it.message!!,Snackbar.LENGTH_LONG).show() }
-                    }
-                    Status.ERROR ->{
-                        binding?.mainLayout?.let { it1 -> Snackbar.make(it1,it.message!!,Snackbar.LENGTH_LONG).show() }
-                    }
-                }
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
-            })
+    var insertCounter: (Counter, () -> Unit) -> Unit = { counter, func ->
+        lifecycleScope.launch {
+            viewModel.createCounter(counter).observe(
+                this@MainActivity,
+                androidx.lifecycle.Observer {
+                    when (it.status) {
+                        Status.SUCCESS -> {
+                            func.invoke()
+                            binding?.mainLayout?.let { it1 ->
+                                Snackbar.make(
+                                    it1,
+                                    it.message!!,
+                                    Snackbar.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                        Status.ERROR -> {
+                            binding?.mainLayout?.let { it1 ->
+                                Snackbar.make(
+                                    it1,
+                                    it.message!!,
+                                    Snackbar.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
+
+                })
         }
 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding?.root
         toolbar = binding!!.toolbarMainactivity
@@ -74,9 +91,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
  //       setListener()
 
+        throw RuntimeException("Test Crash")
+
         callback = {
             makeVisible()
-            Log.d(TAG,"Callback method call")
+            Log.d(TAG, "Callback method call")
         }
 
 //        showNotification(this,"ABC","DEF")
@@ -89,7 +108,7 @@ class MainActivity : AppCompatActivity() {
 
     fun makeVisible(){
         binding.apply {
-            Log.d(TAG,"MAKE VISIBLE")
+            Log.d(TAG, "MAKE VISIBLE")
         //    this?.appBar?.visibility = View.VISIBLE
 //            this?.mainBtmNav?.visibility = View.VISIBLE
         }
