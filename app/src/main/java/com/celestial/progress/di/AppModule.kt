dfbc4fde4e8cb71fa2c5b951b92a208b89217339
waiyanhtat77@gmail.com <waiyan@1cube.my>
2021-10-01 15:28:39 +0630

    Database lock add

--- app/src/main/java/com/celestial/progress/di/AppModule.kt
+++ app/src/main/java/com/celestial/progress/di/AppModule.kt
@@ -1,21 +1,24 @@
 package com.celestial.progress.di
 
+import android.R
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import androidx.room.Room
-import com.celestial.progress.others.Constants
 import com.celestial.progress.data.CounterDao
 import com.celestial.progress.data.CounterDatabase
 import com.celestial.progress.data.CounterRepository
 import com.celestial.progress.data.DefaultRepository
-import com.celestial.progress.widget.ProgressWidgetDataProvider
+import com.celestial.progress.others.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -28,14 +31,24 @@ object AppModule {
     @Singleton
     @Provides
     fun provideCounterDao(
-            database: CounterDatabase
+        database: CounterDatabase
     ) = database.counterDao()
 
     @Singleton
     @Provides
     fun provideCounterDatabase(
         @ApplicationContext context: Context,
-    ) = Room.databaseBuilder(context, CounterDatabase::class.java, Constants.DBNAME).build()
+    ): CounterDatabase {
+        val passString = "helloProgress!"
+
+        val passphrase: ByteArray = SQLiteDatabase.getBytes(passString.toCharArray())
+        val factory = SupportFactory(passphrase)
+
+
+       return Room.databaseBuilder(context, CounterDatabase::class.java, Constants.DBNAME)
+            .openHelperFactory(factory)
+            .build()
+    }
 
 
     @Singleton
@@ -47,7 +60,9 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideSharedPreference(@ApplicationContext context: Context) = PreferenceManager.getDefaultSharedPreferences(context)
+    fun provideSharedPreference(@ApplicationContext context: Context) = PreferenceManager.getDefaultSharedPreferences(
+        context
+    )
 
     @Singleton
     @Provides
